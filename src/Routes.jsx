52794da3e180dd2fee73d7c80d14ff16bb3314c0
alfarissy040 <alfarissy040@gmail.com>
2023-06-05 12:26:22 +0700

    menambah rute ke admin, mengubah logo, bersih bersih

--- src/Routes.jsx
+++ src/Routes.jsx
@@ -1,7 +1,9 @@
 import { createBrowserRouter } from "react-router-dom";
 import App from "./App";
 import MainLayout from "./layout/MainLayout";
+import AdminLayout from "./layout/AdminLayout";
 import Checkout from "./components/Checkout";
+import Dashboard from "./components/admin/Dashboard";
 
 const router = createBrowserRouter([
     {
@@ -15,6 +17,16 @@ const router = createBrowserRouter([
             {
                 path: "checkout",
                 element: <Checkout />,
+            },
+        ],
+    },
+    {
+        path: "/admin",
+        element: <AdminLayout />,
+        children: [
+            {
+                index: true,
+                element: <Dashboard />,
             },
         ],
     },
